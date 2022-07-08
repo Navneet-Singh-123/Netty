9abb050851d03074aad23ce53ae302e1d16d14b0
Navneet Singh <singh.navneet@singhna-ltmcz2r.internal.salesforce.com>
2022-07-08 11:25:52 +0530

    Env fix

--- Bot/bot.py
+++ Bot/bot.py
@@ -5,15 +5,12 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from pathlib import Path
 from dotenv import load_dotenv
-import json
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
-SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
+load_dotenv()
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN') 
+SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')
 
 app = App(token=SLACK_BOT_TOKEN)
-
 
 @app.event("app_home_opened")
 def update_home_tab(client, event, logger):
